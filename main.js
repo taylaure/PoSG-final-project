1a7c2e82db92dde2a4f28f2fbc81107f437f7709
chiujess <chiujess@umich.edu>
2019-06-04 15:03:40 +0200

    worked on map

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 300 },
-            debug: false
+            debug: true
         }
     },
     scene: [loadingScene, homeScene, gameScene],
@@ -52,24 +52,18 @@ gameScene.create = function() {
   this.maze.create(550, 50, 'white_tree').setScale(0.2).refreshBody();
   this.maze.create(580, 70, 'white_tree').setScale(0.2).refreshBody();
   // Start and goal
-  this.house = this.add.sprite(80, 480, 'start');
+  this.house = this.physics.add.sprite(80, 480, 'start');
+  this.house.body.allowGravity = false;
+  this.house.setImmovable();
   this.house.setScale(0.25);
-  this.house = this.add.sprite(700, 80, 'goal');
-  this.house.setScale(0.2);
+  this.goal = this.add.sprite(700, 80, 'goal');
+  this.goal.setScale(0.2);
 
   // Mini games
   this.balloon = this.physics.add.sprite(100, 100, 'balloon');
   this.balloon.body.allowGravity = false;
   this.balloon.setImmovable();
   this.balloon.setScale(0.2);
-
-  /*
-  this.small_bush = this.physics.add.sprite(60, 60, 'bush');
-  this.small_bush.setScale(0.3);
-  this.small_bush.setCollideWorldBounds(true);
-  this.small_bush.body.allowGravity = false;
-  this.small_bush.setImmovable();
-  */
 
   this.player = new Player(this, 100, config.height - 100, cursors);
 
@@ -79,6 +73,7 @@ gameScene.create = function() {
 
   this.physics.add.collider(this.player, this.maze);
   this.physics.add.collider(this.player, this.balloon, onHitBalloon, null, this);
+  this.physics.add.collider(this.player, this.house, onHitHouse, null, this);
   //this.physics.world.collideSpriteVsGroup(this.player, this.maze);
 };
 
@@ -88,8 +83,12 @@ gameScene.update = function(time, delta) {
 
 function onHitBalloon () {
   if(bacteria_touched) {
+    if(this.msgBox) {
+      return;
+    }
+    console.log('messageBox created');
     // Creating a message box
-    var msgBox = this.add.container(400, 300);
+    this.msgBox = this.add.container(400, 300);
     var back = this.add.sprite(0, 0, 'msgBox');
     var back_button = this.add.sprite(280, 80, 'back_button').setScale(0.2);
     //var go_home_text = this.add.text(0, 0, 'Uh oh! You have not clean up your hands yet! Go back home to get them cleaned up!');
@@ -102,14 +101,15 @@ function onHitBalloon () {
       font: '30px Lucida Sans Unicode',
       fill: '#ffffff'
     });
-    msgBox.add(back);
-    msgBox.add(back_button);
-    msgBox.add(go_home_text1);
-    msgBox.add(go_home_text2);
+    this.msgBox.add(back);
+    this.msgBox.add(back_button);
+    this.msgBox.add(go_home_text1);
+    this.msgBox.add(go_home_text2);
 
     back_button.setInteractive();
     back_button.on('pointerdown', function(){
-        msgBox.destroy();
+        this.msgBox.destroy();
+        this.msgBox = undefined;
     }, this);
   }
   else {
@@ -119,6 +119,8 @@ function onHitBalloon () {
   }
 };
 
-function create_message() {
-  
+function onHitHouse () {
+  if(bacteria_touched) {
+    this.scene.start('Wash');
+  }
 }

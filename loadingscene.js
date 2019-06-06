88c85a118e609f6438d788abfbceeb1a4c1df9a9
chiujess <chiujess@umich.edu>
2019-06-06 14:15:12 +0200

    most recent?

--- loadingscene.js
+++ loadingscene.js
@@ -24,10 +24,12 @@ loadingScene.preload = function() {
   this.load.image('palm', 'assets/minigame/palm.png');
   this.load.image('go_button', 'assets/minigame/go_button.png');
   this.load.image('toilet', 'assets/toilet.png');
+  this.load.image('okButton', 'assets/ok.png');
   this.load.audio('meow', 'assets/meow.wav');
   this.load.audio('magic', 'assets/magic.wav');
   this.load.audio('flush', 'assets/flush.wav');
   this.load.audio('click', 'assets/click.wav');
+  this.load.audio('cheer', 'assets/cheer.wav');
 }
 
 loadingScene.create = function() {
--- minigameJs/minigame.js
+++ minigameJs/minigame.js
@@ -43,8 +43,39 @@ minigameScene.update = function (time, delta)
 {
     if (gameOver)
     {
-      this.scene.resume('Game');
-      this.scene.stop();
+      if(this.msgBox) {
+        return;
+      }
+      console.log('Win Game');
+
+      var cheer = this.sound.add('cheer');
+      cheer.play();
+      // Creating a message box
+      this.msgBox = this.add.container(400, 300);
+      var back = this.add.sprite(0, 0, 'msgBox');
+      var ok_button = this.add.sprite(280, 80, 'okButton').setScale(0.2);
+      var place = this.add.sprite(270, -30, 'handSani').setScale(0.2);
+      //var go_home_text = this.add.text(0, 0, 'Uh oh! You have not clean up your hands yet! Go back home to get them cleaned up!');
+      let congrats = this.add.text(-320, -100, 'Congratulations! You have won a hand sanitizer!', {
+          font: '20px Lucida Sans Unicode',
+          fill: '#ffffff',
+          wordWrap: {width: 550, useAdvanceWrap: true}
+      });
+
+      this.msgBox.add(back);
+      this.msgBox.add(place);
+      this.msgBox.add(ok_button);
+      this.msgBox.add(congrats);
+
+      ok_button.setInteractive();
+      ok_button.on('pointerdown', function(){
+          var click = this.sound.add('click');
+          click.play();
+          this.msgBox.destroy();
+          this.msgBox = undefined;
+          this.scene.resume('Game');
+          this.scene.stop();
+      }, this);
       return;
     }
     this.paddle.update(time, delta);
